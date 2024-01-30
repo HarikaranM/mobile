d5dcdcaf745f38d748d9efeb0dba70f7ebc97ef7
HariKaranM <hariharan347vm@gmail.com>
2024-01-30 18:38:45 +0530

    style and html and js  modified

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ $(document).ready(function(){
     $('.fa-bars').click(function(){
         $(this).toggleClass('fa-times');
         $('.navlist').toggleClass('nav-toggle');
+
     });
-    
+     $('.i33').click(function(){
+        $(this).next('.i44').slideToggle();
+        $(this).next('.fa-caret').toggleClass('rotate');
+        
+    });
     });
